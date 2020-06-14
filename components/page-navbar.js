687a2b10e8796d2e68917e461552f7a6ad1a77c4
wargunnerguy <reimovellemaa@gmail.com>
2020-06-14 18:09:55 +0300

    basic page

--- components/page-navbar.js
+++ components/page-navbar.js
@@ -21,9 +21,9 @@ Vue.component('page-navbar', {
                 <!--                <li class="nav-item"><a class="nav-link" href="#section-pricing">Hinnad</a></li>-->
                 <li class="nav-item"><a class="nav-link" href="#section-faq">KKK</a></li>
                 <li class="nav-item cta-btn ml-xl-2 ml-lg-2 ml-md-0 ml-sm-0 ml-0">
-                    <a class="nav-link" href="#calculator" target="_blank">
-                        <span class="pb_rounded-4 px-4">Kalkuleeri hinnavõit</span>
-                    </a>
+<!--                    <a class="nav-link" href="#calculator" target="_blank">-->
+<!--                        <span class="pb_rounded-4 px-4">Kalkuleeri hinnavõit</span>-->
+<!--                    </a>-->
                 </li>
             </ul>
         </div>
