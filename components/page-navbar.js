033e5d69b7b838ad8987b3763c4e2d75f3a11483
wargunnerguy <reimovellemaa@gmail.com>
2020-11-02 22:35:52 +0200

    add hinnakiri

--- components/page-navbar.js
+++ components/page-navbar.js
@@ -18,7 +18,7 @@ Vue.component('page-navbar', {
                 <li class="nav-item"><a class="nav-link" href="#section-home">Pealeht</a></li>
                 <li class="nav-item"><a class="nav-link" href="#section-features">Mida pakume</a></li>
                 <!--                <li class="nav-item"><a class="nav-link" href="#section-reviews">Tehtud tööd</a></li>-->
-                <!--                <li class="nav-item"><a class="nav-link" href="#section-pricing">Hinnad</a></li>-->
+                <li class="nav-item"><a class="nav-link" href="#section-pricing">Hinnakiri</a></li>
                 <li class="nav-item"><a class="nav-link" href="#section-faq">KKK</a></li>
                 <li class="nav-item cta-btn ml-xl-2 ml-lg-2 ml-md-0 ml-sm-0 ml-0">
 <!--                    <a class="nav-link" href="#calculator" target="_blank">-->
